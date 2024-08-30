8cd7a0ee89151b185cd1df4e39844468593bef9c
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-08-30 11:21:43 -0300

    feat: Create interface for create user

--- MyInvestClient/src/app/app.routes.ts
+++ MyInvestClient/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
 
 export const routes: Routes = [
      {
           path: '', component: HomepageComponent
+     },
+     {
+          path: 'account', component: CreateAccountComponent
      }
 ];
---
+++ MyInvestClient/src/app/components/create-account/create-account.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../layout/navbar/navbar.component';
+
+@Component({
+  selector: 'app-create-account',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './create-account.component.html',
+  styleUrl: './create-account.component.scss'
+})
+export class CreateAccountComponent {
+
+}
